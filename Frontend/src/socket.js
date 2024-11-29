6aa933f0ac55b2238613e35d86a7af666ea30cfa
anveshajain19 <anvesha19jain@gmail.com>
2024-11-30 01:08:14 +0530

    update

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
  * @returns {Socket} - A Socket.IO client instance.
  */
 export const initSocket = async () => {
-    const backendURL = "https://group07-major-project00.onrender.com:5000" ;
+    const backendURL = "https://group07-major-project0.onrender.com" ;
     if (!backendURL) {
         console.error("REACT_APP_BACKEND_URL is not defined in the environment variables.");
         throw new Error("Backend URL not provided. Please check your environment configuration.");
